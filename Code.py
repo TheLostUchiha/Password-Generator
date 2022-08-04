ade805d075bbc8f20bb2e262d765d57346833f51
TheLostUchiha <90551799+TheLostUchiha@users.noreply.github.com>
2022-08-04 14:27:37 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,5 +1,4 @@
 # Password-Generator
-Python code for password Generator
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
